35b18f57096f374c32f21d7e911b6ee017ccd678
kdh12345 <go3572@naver.com>
2024-05-18 09:40:41 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/README.md
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 자동차 대여 기록 별 대여 금액 구하기 - 151141 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151141?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/151141?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 05월 18일 09:37:13
+2024년 05월 18일 09:40:40
 
 ### 문제 설명
 
--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,22 +1,27 @@
-WITH value AS (
-    SELECT car.daily_fee, car.car_type, his.history_id,
-           DATEDIFF(end_date, start_date) + 1 AS period,
-    CASE 
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 30 THEN '30일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 7 THEN '7일 이상'
-      ELSE 'NONE' END AS duration_type
-FROM car_rental_company_rental_history AS his
-INNER JOIN car_rental_company_car AS car ON car.car_id = his.car_id
-WHERE car.car_type = '트럭')   
+WITH HIST AS (
+SELECT B.HISTORY_ID AS HISTORY_ID
+     , A.CAR_ID AS CAR_ID
+     , A.CAR_TYPE AS CAR_TYPE
+     , A.DAILY_FEE AS DAILY_FEE
+     , B.DURATION AS DURATION
+     , CASE WHEN B.DURATION < 7 THEN ''
+            ELSE CASE WHEN B.DURATION < 30 THEN '7일 이상'
+            ELSE CASE WHEN B.DURATION < 90 THEN '30일 이상'
+            ELSE '90일 이상'
+            END END END AS DURATION_TYPE
+  FROM CAR_RENTAL_COMPANY_CAR A
+     , (SELECT HISTORY_ID
+             , CAR_ID
+             , END_DATE - START_DATE + 1 AS duration
+  FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY) B
+ WHERE 1 = 1
+   AND A.CAR_ID = B.CAR_ID
+   AND A.CAR_TYPE = '트럭')
 
-
-
-SELECT value.history_id, 
-    ROUND(value.daily_fee * value.period * 
-          (100 - IFNULL(plan.discount_rate,0)) / 100) AS FEE
-FROM value
-LEFT JOIN car_rental_company_discount_plan AS plan 
-    ON plan.duration_type = value.duration_type 
-    AND plan.car_type = value.car_type
-ORDER BY 2 DESC, 1 DESC
+SELECT H.HISTORY_ID
+     , H.DAILY_FEE * H.DURATION * (100 - NVL(A.DISCOUNT_RATE, 0)) / 100 AS FEE
+  FROM HIST H
+      , CAR_RENTAL_COMPANY_DISCOUNT_PLAN A
+  WHERE H.CAR_TYPE = A.CAR_TYPE(+)
+    AND H.DURATION_TYPE = A.DURATION_TYPE(+)
+ ORDER BY H.DAILY_FEE * H.DURATION * (100 - NVL(A.DISCOUNT_RATE, 0)) / 100 desc, H.HISTORY_ID desc
