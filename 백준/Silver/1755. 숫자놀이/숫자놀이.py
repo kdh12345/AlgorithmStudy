127787106c85f07a578319544ac8cdbcd7674454
kdh12345 <go3572@naver.com>
2024-06-04 21:27:33 +0900

    [Silver IV] Title: 숫자놀이, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.py
@@ -0,0 +1,19 @@
+import sys
+
+m,n = map(int,sys.stdin.readline().split())
+
+dic = {'1':'one', '2':'two', '3':'three', '4':'four', '5':'five', '6':'six',
+        '7':'seven', '8':'eight', '9':'nine', '0':'zero'}
+
+arr = []
+
+for i in range(m,n+1):
+    word = ' '.join(dic[j] for j in str(i))
+    arr.append((i,word))
+
+arr.sort(key=lambda x:x[1])
+
+for i in range(len(arr)):
+    if i%10 == 0 and i!=0:
+        print(sep='\n')
+    print(arr[i][0],end=' ')
