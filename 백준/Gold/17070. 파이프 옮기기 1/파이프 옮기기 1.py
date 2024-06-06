23972f253facdbe4e86ec70cd7949d35110adadf
kdh12345 <go3572@naver.com>
2024-06-06 17:08:18 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -7,12 +7,12 @@ def sol():
 
     for x in range(1,n):
         for y in range(1,n):
-            if board[x][y] == 0 and board[x][y-1] == 0 and board[x-1][y] == 0: #diagonal
-                dp[1][x][y] = dp[0][x-1][y-1] + dp[1][x-1][y-1] + dp[2][x-1][y-1]
-
+        
             if board[x][y] == 0: #row col
                 dp[0][x][y] = dp[0][x][y-1] + dp[1][x][y-1]
                 dp[2][x][y] = dp[2][x-1][y] + dp[1][x-1][y]
+            if board[x][y] == 0 and board[x][y-1] == 0 and board[x-1][y] == 0: #diagonal
+                dp[1][x][y] = dp[0][x-1][y-1] + dp[1][x-1][y-1] + dp[2][x-1][y-1]
     result = sum(dp[i][n-1][n-1] for i in range(3))
     return result
 
