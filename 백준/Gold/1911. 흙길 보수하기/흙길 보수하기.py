1db58923af65aa8e5c305d0002e50134523bc94d
kdh12345 <go3572@naver.com>
2024-06-27 22:20:55 +0900

    [Gold V] Title: 흙길 보수하기, Time: 52 ms, Memory: 33164 KB -BaekjoonHub

---
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n,l = map(int,input().split())
+#이차원 배열에 데이터 입력
+puddles=[(list(map(int,input().split()))) for _ in range(n)]
+#puddels[0][]기준으로 오름차순 정렬
+puddles.sort(key=lambda x : x[0])
+result=0
+boundary=puddles[0][0]
+for start, end in puddles:
+    if start>boundary:
+        boundary=start
+    diff = end-boundary
+    if diff%l > 0:
+        count=diff//l+1
+        boundary = end + (l - diff % l)
+    else:
+        count=diff//l
+        boundary = end
+    result+=count
+print(result)
