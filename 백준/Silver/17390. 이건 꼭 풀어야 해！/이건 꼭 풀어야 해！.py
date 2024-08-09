7167c4ac42fa6a0d6cac83cecb7e4962405c4b93
kdh12345 <go3572@naver.com>
2024-08-09 22:13:43 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 336 ms, Memory: 181348 KB -BaekjoonHub

---
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline  
+
+n,q = map(int,input().split())
+numbers = list(map(int,input().split()))
+numbers.sort()
+
+total = [0]
+for i in range(n):
+    total.append(numbers[i]+total[i])
+
+for i in range(q):
+    s,e = map(int,input().split())
+    ans = total[e] - total[s-1]
+    print(ans)
