ae3ce5eb64d7e652d8bc12da4da262bb16533db9
kdh12345 <go3572@naver.com>
2024-09-19 21:27:16 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+dp = [float(input()) for _ in range(n)]
+
+for i in range(1,n):
+    dp[i]=max(dp[i],dp[i]*dp[i-1])
+print('%0.3f'% max(dp))
