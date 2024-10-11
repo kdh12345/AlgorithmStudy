f613c8cbcf75b8deba66f59f5055e174489d6cfc
kdh12345 <go3572@naver.com>
2024-10-11 21:00:45 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCP 기출문제] 1번 / 동영상 재생기 - 340213 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340213) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 0.20 ms
+메모리: 10.3 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 01일 18:14:15
+2024년 10월 11일 21:00:42
 
 ### 문제 설명
 
--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -3,29 +3,31 @@ def solution(video_len, pos, op_start, op_end, commands):
     if op_start <= pos <= op_end:
         pos = op_end
     for c in commands:
-        if c == "prev":
+        if c == 'prev':
             tmp = pos.split(':')
             m = int(tmp[0])
             s = int(tmp[1])
-            if (s - 10 < 0) and (m >= 1):
+            if (s-10<0) and (m>=1):
                 m -= 1
                 s = (s-10)+60
             else:
                 s = max(s-10,0)
-        elif c == "next":
+        
+        elif c == 'next':
             tmp = pos.split(':')
             m = int(tmp[0])
             s = int(tmp[1])
+            
             tmp2 = video_len.split(':')
             m2 = int(tmp2[0])
             s2 = int(tmp2[1])
-            if (s+10 >= 60) and (m < m2):
+            if (s+10>=60) and (m<m2):
                 m += 1
             if m == m2:
                 s = min((s+10)%60,s2)
             else:
                 s = (s+10)%60
-        pos = str(m).zfill(2)+":"+str(s).zfill(2)
+        pos = str(m).zfill(2)+':'+str(s).zfill(2)
         if op_start <= pos <= op_end:
             pos = op_end
     answer = pos
