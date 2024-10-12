53fbf920d54725d2a4b693b79ecce7daffc1f55c
kdh12345 <go3572@naver.com>
2024-10-12 13:00:10 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,14 @@
+def solution(brown, yellow):
+    answer = []
+    yx = 0
+    yy = 0
+    for i in range(1,yellow+1):
+        if yellow%i == 0:
+            yx = yellow // i
+            yy = i
+            if 2*yx+2*yy+4 == brown:
+                answer.append(yx+2)
+                answer.append(yy+2)
+                break
+            #answer.sort(reverse=True)
+    return answer
