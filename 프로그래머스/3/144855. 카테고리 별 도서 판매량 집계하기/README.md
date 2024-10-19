28ca521b5f477923d86d4e46404a341108636572
kdh12345 <go3572@naver.com>
2024-10-19 14:11:46 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/README.md
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 카테고리 별 도서 판매량 집계하기 - 144855 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144855?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/144855?language=oracle) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 02일 14:50:12
+2024년 10월 19일 14:11:44
 
 ### 문제 설명
 
--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,10 +1,10 @@
 -- 코드를 입력하세요
-SELECT A.CATEGORY
-      ,SUM(B.SALES) AS TOTAL_SALES
+SELECT A.category
+      ,sum(B.sales) as total_sales
   FROM BOOK A
-     ,BOOK_SALES B
-WHERE A.BOOK_ID = B.BOOK_ID
-  AND YEAR(SALES_DATE) = '2022'
-  AND MONTH(SALES_DATE) = '01'
-GROUP BY A.CATEGORY
-ORDER BY A.CATEGORY
+      ,BOOK_SALES B
+where A.BOOK_ID = B.BOOK_ID
+  and TO_CHAR(B.SALES_DATE,'YYYYMM') = '202201'
+group by A.category
+order by A.category
+;
