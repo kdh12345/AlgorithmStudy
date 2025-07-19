3edb35c0096285f9664fda825f67a5fed83bd8b4
kdh12345 <go3572@naver.com>
2025-07-19 12:22:17 +0900

    [level 2] Title: 주식가격, Time: 30.45 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,24 +1,11 @@
-from collections import deque
 def solution(prices):
-    answer = []
-    """q = deque(prices)
-    while q:
-        price = q.popleft()
-        sec = 0
-        for item in q:
-            sec+=1
-            if price > item:
-                break            
-        answer.append(sec)
-        sec = 0"""
-    
     length = len(prices)
-    answer = [ i for i in range(length-1,-1,-1)]
-    st = [0]
-    for i in range(1,length):
+    answer = [ i for i in range (length-1, -1, -1)]
+    
+    st =[0]
+    for i in range(1,len(prices)):
         while st and prices[st[-1]] > prices[i]:
             j = st.pop()
             answer[j] = i - j
         st.append(i)
-        
     return answer
